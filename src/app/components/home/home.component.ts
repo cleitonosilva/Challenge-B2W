f240509ecaf66fcecf86d7ab4525f72b91951571
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-24 18:04:15 -0300

    populando home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/user.model';
+import { UsersService } from 'src/app/services/users.service';
+import { userMockData } from 'src/app/utils/userMock';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  usuario: Usuario
+  
 
-  constructor() { }
+  constructor(private usersService : UsersService) { }
 
   ngOnInit(): void {
+
+    this.usuario = userMockData
+    console.log(this.usuario)
+
+    // this.usersService.getUsuarios().subscribe((x: any) =>
+    // {this.usuario = x.results[0];
+      
+    // } )
+      
   }
 
 }
