20c39287c4f5aa76154a2c9faedbf0cc0e619cc9
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-24 18:03:54 -0300

    Objeto inicial da aplicação

---
+++ src/app/utils/userMock.ts
@@ -0,0 +1,66 @@
+const dados = {
+  age : 32, 
+  date : "15/09/1989", 
+  };
+const id = {
+  name : "cleiton",
+  value : "cleiton1509"
+};
+const location = {
+  city : "são joão de meriti",
+  coordinates : {
+    latitude : "015202100",
+    longitude : "50155501",
+  }, 
+  country : "Rio de Janeiro",
+  postcode : 25510210,
+  state : "Rio de Janeiro",
+  street : {
+    name: "rua doutor mario cabral",
+    number: 210
+  },
+  timezone : {
+    description : "GMT-Brasislia",
+    offset: "-08:00",
+  }
+};
+const login = {
+  md5 : "50114050", 
+  password : "50114050", 
+  sha1 : "50114050", 
+  sha256 : "50114050", 
+  username : "50114050", 
+  uuid : "50114050", 
+};
+const name = {
+  first : "Cleiton", 
+  last : "Silva",
+  title : "Srs", 
+};
+const picture = {
+  large : "https://randomuser.me/api/portraits/men/39.jpg",
+  medium : "https://randomuser.me/api/portraits/med/men/39.jpg",
+  thumbnail : "https://randomuser.me/api/portraits/thumb/men/39.jpg" 
+};
+const register = {
+  age: 12,
+  date: "10/02/2008",
+}
+
+
+export const userMockData = {
+  cell: "21964099980",
+  dados : dados,
+  email: "cleiton.o.dev@gmail.com",
+  gender: "masc",
+  id: id,
+  location: location,
+  login: login,
+  name : name,
+  nat: "braziliam",
+  phone: "21964099980",
+  picture: picture,
+  registered: register
+} 
+
+
